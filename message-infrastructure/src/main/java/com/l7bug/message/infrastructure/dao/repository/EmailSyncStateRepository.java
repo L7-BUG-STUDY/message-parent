a365cba2f547e88d443ed0e5cc3c114b7251fcd5
L7-BUG <ll789y@gmail.com>
2026-01-08 17:57:09 +0800

    feat(email): 添加邮件同步状态数据对象和配置
    - 创建 EmailSyncState 数据对象用于跟踪邮件同步状态
    - 添加 username、folder、uid_validity 和 uid 字段
    - 配置 JPA 实体映射和数据库表结构
    - 在 shardingsphere-config-mock.yaml 中添加分片数据源配置
    - 修改 message_email_record 表的分片数据节点配置
    - 在 table.sql 中创建 message_email_sync_state 表结构
    - 添加系统用户用户名唯一索引和邮件同步点位表注释

--- message-infrastructure/src/main/java/com/l7bug/message/infrastructure/dao/repository/EmailSyncStateRepository.java
+++ message-infrastructure/src/main/java/com/l7bug/message/infrastructure/dao/repository/EmailSyncStateRepository.java
@@ -1,10 +1,15 @@
 package com.l7bug.message.infrastructure.dao.repository;
 
+import com.l7bug.message.infrastructure.dao.dataobject.EmailSyncState;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  * EmailSyncStateRepository
  *
  * @author Administrator
  * @since 2026/1/8 17:53
  */
-public interface EmailSyncStateRepository {
+@Repository
+public interface EmailSyncStateRepository extends JpaRepository<EmailSyncState, Long> {
 }
