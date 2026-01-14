4aa3cfee45f489eb7263f12ef7c8a0cf823ab899
L7-BUG <ll789y@gmail.com>
2026-01-14 18:20:12 +0800

    refactor(consul): 更新文件服务客户端引用
    - 将RemoteFileService替换为FileClient
    - 更新IndexController中的依赖注入
    - 修改hello方法中的服务调用
    - 更新image方法中的下载功能调用

--- consul-openfeign/src/main/java/com/l7bug/consul/IndexController.java
+++ consul-openfeign/src/main/java/com/l7bug/consul/IndexController.java
@@ -3,7 +3,7 @@ package com.l7bug.consul;
 import cn.hutool.core.io.IoUtil;
 import com.l7bug.common.result.Result;
 import com.l7bug.consul.feign.DemoClient;
-import com.l7bug.file.client.RemoteFileService;
+import com.l7bug.file.client.FileClient;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,16 +23,16 @@ import java.io.IOException;
 public class IndexController {
 	private final DemoClient demoClient;
 
-	private final RemoteFileService remoteFileService;
+	private final FileClient fileClient;
 
 	@GetMapping
 	public String hello() {
-		return "hello world" + remoteFileService.hello().getData();
+		return "hello world" + fileClient.hello().getData();
 	}
 
 	@GetMapping("/image")
 	public void image(HttpServletResponse response) throws IOException {
-		Result<byte[]> download = this.remoteFileService.download(1L);
+		Result<byte[]> download = this.fileClient.download(1L);
 		IoUtil.copy(new ByteArrayInputStream(download.getData()), response.getOutputStream());
 	}
 }
